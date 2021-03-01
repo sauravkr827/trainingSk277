134e884bb926636e8364833819acb4e9525dcd42
AnuradhT2018-PC\saurav <sauravkr82711@gmail.com>
2021-03-01 17:49:30 +0530

    first commit

---
+++ trainingSk277core/src/de/hybris/trainingSk277/core/customInterceptor/ManufacturerDetailsLoadInterceptor.java
@@ -0,0 +1,12 @@
+package de.hybris.trainingSk277.core.customInterceptor;
+
+import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
+import de.hybris.platform.servicelayer.interceptor.InterceptorException;
+import de.hybris.platform.servicelayer.interceptor.LoadInterceptor;
+
+public class ManufacturerDetailsLoadInterceptor implements LoadInterceptor {
+    @Override
+    public void onLoad(Object o, InterceptorContext interceptorContext) throws InterceptorException {
+
+    }
+}
